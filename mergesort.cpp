7a7e80c753ef3e0abc24bf782041a7af04d8c085
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-04-15 11:09:10 +0700

    add input function

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,6 +7,28 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movements
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n------------------" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
+
 
 
 
